14066b7cc7ff483c14e306f76d01f7b9021f869d
Dario Skidelsky <76440563+darioque@users.noreply.github.com>
2022-10-04 21:27:43 -0300

    added recursion to ex 1 and 2 + changed variable names as requested

--- ppr-haskell/tp2 2022.hs
+++ ppr-haskell/tp2 2022.hs
@@ -1,27 +1,38 @@
 esDivisible :: Int -> Int -> Bool
-esDivisible x y | y == 0 = False
-				| x `mod` y == 0 = True
+esDivisible nro exp | exp == 0 = False
+				| nro `mod` exp == 0 = True
 				| otherwise = False
 
--- chequear clase de variable x e y
-
-
+-- listaFactoresDivisiblesDe :: Int -> [Int]
+-- listaFactoresDivisiblesDe x = [y | y <- [1..x], esDivisible x y]
 
 listaFactoresDivisiblesDe :: Int -> [Int]
-listaFactoresDivisiblesDe x = [y | y <- [1..x], esDivisible x y]
+listaFactoresDivisiblesDe n = listaFactoresDivisiblesDe' n 1
+	where listaFactoresDivisiblesDe' n y | y > n = []
+									     | esDivisible n y = y : listaFactoresDivisiblesDe' n (y+1)
+									     | otherwise = listaFactoresDivisiblesDe' n (y+1)
+
+
+-- esPrimo :: Int -> Bool
+-- esPrimo x | length (listaFactoresDivisiblesDe x) == 2 = True
+-- 		  | otherwise = False
 
 esPrimo :: Int -> Bool
-esPrimo x | length (listaFactoresDivisiblesDe x) == 2 = True
-		  | otherwise = False
+esPrimo 1 = False
+esPrimo 2 = True
+esPrimo nro = esPrimo' nro 2
+  where esPrimo' nro i | i == nro = True
+	                 | nro `mod` i == 0 = False
+	             	 | otherwise = esPrimo' nro (i + 1)
+
 
 listadoPrimosMenoresOIgualesQue :: Int -> [Int]
-listadoPrimosMenoresOIgualesQue x = [y | y <- [1..x], esPrimo y]
-
+listadoPrimosMenoresOIgualesQue n = [y | y <- [1..n], esPrimo y]
 
 factorizacion :: Int -> [Int]
 factorizacion 1 = []
-factorizacion x = if esPrimo num then num : factorizacion (x `div` num) else x : []
-				  where num  = head (tail (listaFactoresDivisiblesDe x))
+factorizacion n = if esPrimo num then num : factorizacion (n `div` num) else n : []
+				  where num  = head (tail (listaFactoresDivisiblesDe n))
 
 expansion :: [(Int, Int)] -> Int
 expansion [] = 1
